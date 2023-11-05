956902059840c0e27a0f14b7cfd23851d1858c3b
Guilherme H <ghpm99@gmail.com>
2023-11-05 00:45:28 -0300

    Feat: Create basic struct

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
+from io import BufferedReader
+
+
+class SaveStruct:
+
+    dig_verify = b''
+    dump_random = b''
+    name = b''
+    level = b''
+    exp = b''
+    strength = b''
+
+    def __init__(self, file: BufferedReader):
+        self.dig_verify = file.read(16)
+        self.dump_random = file.read(73)
+        self.name = file.read(6)
+
+    def print_name(self):
+        print(self.name)
+
+
 file = open("save/SAVE.BIN", "rb")
-content = file.read()
-print(content[0x001d:66])
-print(chr(0x63))
-print(content[0:1])
-print(b'\x63')
-print(int('0x63', 16))
-print(int('0x3c', 16))
+print(file.read(16))
+
+save = SaveStruct(file)
+save.print_name()
+
 file.close()
+
+# data[0xb22b] = 116
+# print(data[0xb229:111].hex())
+
+# file_write = open("save/edited/SAVE.BIN", "wb")
+# file_write.write(data)
+# file_write.close()
